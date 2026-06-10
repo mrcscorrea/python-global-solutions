c904c3401f168faa71c46f177a6d758d39b6d391
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-06-10 00:01:04 -0300

    Update main.py

--- codigo/main.py
+++ codigo/main.py
@@ -22,7 +22,7 @@
 #  Ele interage com o módulo de dados e executa a lógica principal
 #  do sistema de alerta de incêndios.
 # ===========================================================================
-from codigo.dados import REGIOES
+from dados import REGIOES
 
 # ── Constantes de exibição (Visualização do código) ────────────────────────────
 LINHA_DUPLA  = "=" * 72
